c7819ada91b1a0697a9dcaadd9ca9ac8969dd96e
Lorenzzo da Mata <maraninilorenzzo@gmail.com>
2026-09-05 15:52:47 -0300

    Adiciona exercício 1

---
+++ Exercicio1/src/Exercicio1.java
@@ -0,0 +1,13 @@
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 211; i <= 250; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
